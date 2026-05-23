01e280052e8a133c4b6d4e549b2c96702d5fdda9
André Keßler <andre.kessler91@gmx.de>
2026-05-23 19:34:50 +0200

    Improved code to trigger button
    Button was moved from D2 to D0

--- esp32-incubator-platformio/src/main.cpp
+++ esp32-incubator-platformio/src/main.cpp
@@ -6,8 +6,6 @@
 #include <multi_channel_relay.h>
 #include <U8g2lib.h>
 #include <SPI.h>
-#include <OneWire.h>
-#include <DallasTemperature.h>
 #include "HX711.h"
 #include <ADXL345.h>
 #include <math.h>
@@ -18,12 +16,9 @@ HX711 scale;
 Multi_Channel_Relay relay;
 ADXL345 adxl;
 SensirionI2cScd4x sensor;
-OneWire oneWire(1);
-DallasTemperature sensors(&oneWire);
 
 static int16_t error;
 static char errorMessage[64];
-int lastState = HIGH;
 byte WTR_LVL_lowData[8];
 byte WTR_LVL_highData[12];
 
@@ -34,23 +29,20 @@ byte WTR_LVL_highData[12];
 
 const byte BUZZER_PIN = D7;
 const byte HUMID_PIN = A9;
-const int buttonPin = D2;
+const int buttonPin = D0;
 const int DT_PIN = 3;
 const int SCK_PIN = 2;
 const float HX711_CALIBRATION_FACTOR = 112.0392;
 const int soundSensorPinAdc = A8;
 
 TaskHandle_t soundTaskHandle = NULL;
+TaskHandle_t sensorTaskHandle = NULL;
 
-#define DS18B20_PIN 1
 #define WTR_LVL_THRESHOLD 100
 #define WTR_LVL_HIGH_ADDR 0x78
 #define WTR_LVL_LOW_ADDR 0x77
 
 void initializeSCD41();
-void printMeasurement(uint16_t co2, float temp, float rh);
-void measureDS18B20();
-void measureAccelerometer();
 void testRelay();
 bool isButtonPressed();
 void playSoundNotification();
@@ -58,6 +50,7 @@ void WTR_LVL_readBytes(byte addr, byte *buf, byte len);
 int WTR_LVL_readPercent();
 int WTR_LVL_readPercentStable(uint8_t samples = 15, uint16_t delayMs = 120);
 void soundRecorderTask(void *parameter);
+void sensorReadingTask(void *parameter);
 
 void setup() {
   Serial.begin(9600);
@@ -86,11 +79,8 @@ void setup() {
   Serial.print(WTR_LVL_readPercentStable(20, 150));
   Serial.println("%");
 
-  pinMode(buttonPin, INPUT);
+  pinMode(buttonPin, INPUT_PULLUP);
   Serial.println("Input button initialized");
-
-  sensors.begin();
-  Serial.println("DS18B20 initialized");
 
   adxl.powerOn();
   Serial.println("ADXL345 Accelerometer initialized");
@@ -127,6 +117,16 @@ void setup() {
     1
   );
 
+  xTaskCreatePinnedToCore(
+    sensorReadingTask,
+    "SensorReading",
+    8192,
+    NULL,
+    1,
+    &sensorTaskHandle,
+    1
+  );
+
   Serial.println("Setup complete!");
 }
 
@@ -135,51 +135,64 @@ void loop() {
     Serial.println("Button pressed!");
     playSoundNotification();
   }
+  vTaskDelay(pdMS_TO_TICKS(50));
+}
 
-  uint16_t co2 = 0;
-  float temp = 0.0;
-  float rh = 0.0;
+void sensorReadingTask(void *parameter) {
+  const unsigned long intervalMs = 15000;
+  for (;;) {
+    unsigned long cycleStart = millis();
+    // --- collect all values first ---
+    float vBus        = ina226.getBusVoltage_V();
+    float current_mA  = ina226.getCurrent_mA();
+    long  lux         = TSL2561.readVisibleLux();
 
-  float vBus = ina226.getBusVoltage_V();
-  float current_mA = ina226.getCurrent_mA();
-  Serial.print("Voltage: ");
-  Serial.print(vBus);
-  Serial.println(" V");
-  Serial.print("Current: ");
-  Serial.print(current_mA / 1000.0);
-  Serial.println(" A");
+    uint16_t co2 = 0;
+    float    co2Temp = 0.0;
+    float    rh = 0.0;
+    char     scdErrMsg[64] = {0};
+    int16_t  scdError = sensor.measureAndReadSingleShot(co2, co2Temp, rh);
+    if (scdError != NO_ERROR) {
+      errorToString(scdError, scdErrMsg, sizeof(scdErrMsg));
+    }
 
-  Serial.print("The Light value is: ");
-  Serial.println(TSL2561.readVisibleLux());
+    double xyz[3];
+    adxl.getAcceleration(xyz);
+    float pitch = atan2((float)xyz[0], sqrt((float)(xyz[1]*xyz[1] + xyz[2]*xyz[2]))) * 180.0f / PI;
+    float roll  = atan2((float)xyz[1], sqrt((float)(xyz[0]*xyz[0] + xyz[2]*xyz[2]))) * 180.0f / PI;
 
-  error = sensor.measureAndReadSingleShot(co2, temp, rh);
-  if (error != NO_ERROR) {
-    Serial.print("Error trying to execute measureAndReadSingleShot(): ");
-    errorToString(error, errorMessage, sizeof(errorMessage));
-    Serial.println(errorMessage);
-  } else {
-    printMeasurement(co2, temp, rh);
+    int   waterLevel = WTR_LVL_readPercentStable(9, 100);
+    float weight     = scale.get_units(10);
+
+    // --- print everything as one block ---
+    Serial.println("\n--- Sensor Reading ---");
+    Serial.print("Voltage:      "); Serial.print(vBus);              Serial.println(" V");
+    Serial.print("Current:      "); Serial.print(current_mA / 1000.0); Serial.println(" A");
+    Serial.print("Light:        "); Serial.print(lux);               Serial.println(" lux");
+    if (scdError == NO_ERROR) {
+      Serial.print("CO2:          "); Serial.print(co2);             Serial.println(" ppm");
+      Serial.print("Temp (SCD41): "); Serial.print(co2Temp);         Serial.println(" °C");
+      Serial.print("Humidity:     "); Serial.print(rh);              Serial.println(" %RH");
+    } else {
+      Serial.print("SCD41 error:  "); Serial.println(scdErrMsg);
+    }
+    Serial.print("Pitch:        "); Serial.print(pitch, 2);          Serial.println(" deg");
+    Serial.print("Roll:         "); Serial.print(roll, 2);           Serial.println(" deg");
+    Serial.print("Water level:  "); Serial.print(waterLevel);        Serial.println(" %");
+    Serial.print("Weight:       "); Serial.print(weight, 1);         Serial.println(" g");
+    Serial.println("----------------------");
+
+    u8g2.firstPage();
+    do {
+      u8g2.setFont(u8g2_font_ncenB10_tr);
+      u8g2.drawStr(0, 24, "Incubator running!");
+    } while (u8g2.nextPage());
+
+    unsigned long elapsed = millis() - cycleStart;
+    if (elapsed < intervalMs) {
+      vTaskDelay(pdMS_TO_TICKS(intervalMs - elapsed));
+    }
   }
-
-  measureDS18B20();
-  measureAccelerometer();
-
-  Serial.print("Water level: ");
-  Serial.print(WTR_LVL_readPercentStable(9, 100));
-  Serial.println("%");
-
-  float weight = scale.get_units(10);
-  Serial.print("Weight: ");
-  Serial.print(weight, 1);
-  Serial.println(" g");
-
-  u8g2.firstPage();
-  do {
-    u8g2.setFont(u8g2_font_ncenB10_tr);
-    u8g2.drawStr(0, 24, "Incubator running!");
-  } while (u8g2.nextPage());
-
-  delay(2000);
 }
 
 void soundRecorderTask(void *parameter) {
@@ -226,47 +239,6 @@ void testRelay() {
   relay.turn_off_channel(4);
 }
 
-void measureDS18B20() {
-  Serial.print("Requesting temperatures from DS18B20");
-  sensors.requestTemperatures();
-  float tempC = sensors.getTempCByIndex(0);
-
-  if (tempC != DEVICE_DISCONNECTED_C) {
-    Serial.print("Temperature for DS18B20 is: ");
-    Serial.println(tempC);
-  } else {
-    Serial.println("Error: Could not read temperature data from DS18B20");
-  }
-}
-
-void measureAccelerometer() {
-  double xyz[3];
-  adxl.getAcceleration(xyz);
-
-  float ax = xyz[0];
-  float ay = xyz[1];
-  float az = xyz[2];
-
-  float pitch = atan2(ax, sqrt(ay * ay + az * az)) * 180.0 / PI;
-  float roll = atan2(ay, sqrt(ax * ax + az * az)) * 180.0 / PI;
-
-  Serial.print("Pitch = ");
-  Serial.print(pitch, 2);
-  Serial.println(" deg");
-
-  Serial.print("Roll = ");
-  Serial.print(roll, 2);
-  Serial.println(" deg");
-}
-
-void printMeasurement(uint16_t co2, float temp, float rh) {
-  Serial.print("CO2 concentration [ppm]: ");
-  Serial.println(co2);
-  Serial.print("Temperature [°C]: ");
-  Serial.println(temp);
-  Serial.print("Relative Humidity [RH]: ");
-  Serial.println(rh);
-}
 
 void initializeSCD41() {
   sensor.begin(Wire, SCD41_I2C_ADDR_62);
@@ -303,10 +275,28 @@ void initializeSCD41() {
 }
 
 bool isButtonPressed() {
+  static unsigned long pressStart = 0;
+  static bool alreadyFired = false;
+  static bool seenHighOnce = false;
+
   int state = digitalRead(buttonPin);
-  bool pressed = (lastState == LOW && state == HIGH);
-  lastState = state;
-  return pressed;
+
+  if (!seenHighOnce) {
+    if (state == HIGH) seenHighOnce = true;
+    return false;
+  }
+
+  if (state == LOW) {
+    if (pressStart == 0) pressStart = millis();
+    if (!alreadyFired && millis() - pressStart >= 1000) {
+      alreadyFired = true;
+      return true;
+    }
+  } else {
+    pressStart = 0;
+    alreadyFired = false;
+  }
+  return false;
 }
 
 void playSoundNotification() {
